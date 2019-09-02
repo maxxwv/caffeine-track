6604213a30985b5440acafa0f7b945123460e2b8
Alex Wharff <git@maxxwv.com>
2019-09-02 17:07:15 -0400

    Update initial data gathering to provide defaults for new users.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,13 +26,12 @@ class HomeController extends Controller
     public function index(Request $request)
     {
         $today = new \DateTime('today', new \DateTimeZone($request->user()->time_zone));
-        $diary = CaffeineTrack::select('drinks.name', 'drinks.id', 'caffeine_content as caffeine', 'drinks.descr', 'drinks.servings as servings_per_container', 'caffeine_tracking.servings as servings_had', 'caffeine_tracking.created_at as date_ingested', 'users.max_caffeine')
+        $diary = CaffeineTrack::select('drinks.name', 'drinks.id', 'caffeine_content as caffeine', 'drinks.descr', 'drinks.servings as servings_per_container', 'caffeine_tracking.servings as servings_had', 'caffeine_tracking.created_at as date_ingested')
             ->leftJoin('drinks', 'drink_id', '=', 'drinks.id')
-            ->leftJoin('users', 'user_id', '=', 'users.id')
             ->whereDate('caffeine_tracking.created_at', '=', $today->format('Y-m-d'))
             ->where('user_id', $request->user()->id)
             ->get();
-        $overview = $this->collateDiary($diary);
+        $overview = $this->collateDiary($diary, $request);
         return view('home', [
             'diary' => $diary,
             'overview' => $overview,
@@ -43,14 +42,17 @@ class HomeController extends Controller
      * Create the overview - we'll figure out how much _total_ caffeine the user has had
      * per drink, then subtract that from their personal max caffeine amount.
      */
-    private function collateDiary($diary){
+    private function collateDiary($diary, Request $request){
+        if($diary->isEmpty()){
+            $diary = Drink::all();
+        }
         $res = [];
         foreach($diary as $drink){
-            $shot = $drink->servings_had * $drink->caffeine;
+            $shot = !empty($drink->servings_had) ? $drink->servings_had * $drink->caffeine : 0;
             $res[$drink->id]['name'] = $drink->name;
             $res[$drink->id]['drink_id'] = $drink->id;
             $res[$drink->id]['total_caffeine_ingested'] = !empty($res[$drink->id]['total_caffeine_ingested']) ? $res[$drink->id]['total_caffeine_ingested'] + $shot : $shot;
-            $res[$drink->id]['left'] = !empty($res[$drink->id]['left']) ? $res[$drink->id]['left'] - $shot : $drink->max_caffeine - $shot;
+            $res[$drink->id]['left'] = !empty($res[$drink->id]['left']) ? $res[$drink->id]['left'] - $shot : $request->user()->max_caffeine - $shot;
             $res[$drink->id]['class_name'] = $res[$drink->id]['left'] <= 0 ? " alert alert-danger" : "";
         }
         return $res;
