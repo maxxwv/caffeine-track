407c34b1512ff9da3366dc037f7dc985f3bad672
Alex Wharff <git@maxxwv.com>
2019-10-13 11:03:34 -0400

    Drop Laradock for my own docker-compose file, update the config values and now everything works perfectly.

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://mjfreeway.local:8000'),
 
     'asset_url' => env('ASSET_URL', null),
 
